b02097e6993d4e57ce298a867d2d7b446e63acaa
jeldikk <jeldi.kamal2013@gmail.com>
2026-03-19 00:56:57 +0530

    some random change

--- apps/webapp/app/page.tsx
+++ apps/webapp/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <h1 className="text-4xl font-bold text-center">
-        Welcome to My Portfolio web page
+        Welcome to Portfolio web page of Kamal Kumar
       </h1>
       <p className="text-lg text-center max-w-2xl">
         This Page is under construction
