198d9afb3c449e69bd191ebac884f798c4d8ff83
jeldikk <jeldi.kamal2013@gmail.com>
2026-03-20 23:43:18 +0530

    feat: enhance header component with responsive drawer and theme switcher integration

--- apps/webapp/components/header/header.component.tsx
+++ apps/webapp/components/header/header.component.tsx
@@ -12,17 +12,73 @@ export default function Header() {
   const dispatch = useAppDispatch();
 
   return (
-    <header className="sticky top-0 z-10 navbar shadow-sm">
-      <div className="flex-1">
-        <Link className="btn btn-ghost text-xl" href="/">
-          Jeldikk
-        </Link>
+    <header className="drawer">
+      <input
+        id="navbar-drawer-toggle"
+        type="checkbox"
+        className="drawer-toggle"
+      />
+      <div className="drawer-content flex flex-col">
+        {/* Navbar */}
+        <div className="navbar bg-base-300 w-full">
+          <div className="flex-none lg:hidden">
+            <label
+              htmlFor="navbar-drawer-toggle"
+              aria-label="open sidebar"
+              className="btn btn-square btn-ghost"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                className="inline-block h-6 w-6 stroke-current"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h16M4 18h16"
+                ></path>
+              </svg>
+            </label>
+          </div>
+          <div className="mx-2 flex-1 px-2">
+            <Link className="btn btn-ghost text-xl" href="/">
+              Jeldikk
+            </Link>
+          </div>
+          <div className="flex-none">
+            {/* Here is the Navbar Items */}
+            <ul className="menu menu-horizontal">
+              <li>
+                <ThemeSwitcher />
+              </li>
+              <li className="hidden lg:block">
+                <Link href={"/"}>Home</Link>
+              </li>
+              <li className="hidden lg:block">
+                <Link href={"/admin"}>Admin</Link>
+              </li>
+
+              {authDetails && isAdmin ? (
+                <button
+                  className="btn btn-sm btn-link font-bold hidden lg:block"
+                  onClick={() => dispatch(logOutUser())}
+                >
+                  Logout
+                </button>
+              ) : null}
+            </ul>
+          </div>
+        </div>
       </div>
-      <div className="flex-none">
-        <ul className="menu menu-horizontal px-1">
-          <li>
-            <ThemeSwitcher />
-          </li>
+      <div className="drawer-side">
+        <label
+          htmlFor="navbar-drawer-toggle"
+          aria-label="close sidebar"
+          className="drawer-overlay"
+        ></label>
+        <ul className="menu bg-base-200 min-h-full w-80 p-4">
           <li>
             <Link href={"/"}>Home</Link>
           </li>
