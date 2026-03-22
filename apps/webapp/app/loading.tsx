a2d1dd3fe4077d05af9fc306058c4b6e33e39d14
J Kamal Kumar <jeldi.kamal2013@gmail.com>
2026-03-22 18:23:08 +0530

    feat: add loading component for improved user experience during data fetching

---
+++ apps/webapp/app/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="flex h-screen w-full items-center justify-center">
+      <span className="loading loading-bars loading-xl"></span>
+    </div>
+  );
+}
