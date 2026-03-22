4ef04706632df713d58067c28bffe6c9ef1a8eaf
J Kamal Kumar <jeldi.kamal2013@gmail.com>
2026-03-22 22:36:19 +0530

    feat: update META_DATA with improved title and description for better clarity

--- apps/webapp/utils/metadata.constants.ts
+++ apps/webapp/utils/metadata.constants.ts
@@ -1,9 +1,9 @@
 import { Metadata } from "next";
 
 export const META_DATA: Metadata = {
-  title: "Senior TypeScript Developer & Full Stack Engineer in MERN",
+  title: "Kamal Kumar Jeldi | Senior MERN Full Stack Developer.",
   description:
-    "Senior Software Engineer specializing in TypeScript, MERN stack, React, Node.js, Playwright automation and AWS.",
+    "A Senior Software Engineer by Profession with 10+ years of experience specializing in TypeScript, MERN stack, React, Node.js, Playwright automation and AWS.",
   keywords: [
     "Senior Software Engineer",
     "Full Stack Developer",
@@ -18,9 +18,9 @@ export const META_DATA: Metadata = {
     google: process.env.GOOGLE_SITE_VERIFICATION_ID,
   },
   openGraph: {
-    title: "Senior TypeScript Developer & Full Stack Engineer in MERN",
+    title: "Kamal Kumar Jeldi | Senior MERN Full Stack Developer.",
     description:
-      "Senior Software Engineer specializing in TypeScript, MERN stack, React, Node.js, Playwright automation, and AWS.",
+      "A Senior Software Engineer by Profession with 10+ years of experience specializing in TypeScript, MERN stack, React, Node.js, Playwright automation and AWS.",
     url: process.env.NEXT_PUBLIC_SITE_URL,
     siteName: "jeldikk.fyi",
     locale: "en_US",
