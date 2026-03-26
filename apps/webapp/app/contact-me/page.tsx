818ae66f3b39ca349f9b7cb4f0caed78d58a77d4
J Kamal Kumar <jeldi.kamal2013@gmail.com>
2026-03-26 12:08:45 +0530

    feat: add construction alert to Contact Me page

--- apps/webapp/app/contact-me/page.tsx
+++ apps/webapp/app/contact-me/page.tsx
@@ -1,6 +1,9 @@
 export default function ContactMe() {
   return (
     <div className="min-h-screen md:max-w-10/12 mx-auto flex flex-col">
+      <div role="alert" className="alert alert-warning m-2">
+        <span>This Page is still under construction</span>
+      </div>
       <h1 className="text-2xl md:text-4xl font-bold text-center mt-10">
         Contact Me
       </h1>
